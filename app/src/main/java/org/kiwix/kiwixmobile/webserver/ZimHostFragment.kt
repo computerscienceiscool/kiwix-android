d595ca52529f6f3192606fb5518deb949d9561a4
MohitMali <futuretechnoworld.mohit@gmail.com>
2023-09-22 11:04:47 +0530

    Fixed Hosted Books don't update on Application

--- app/src/main/java/org/kiwix/kiwixmobile/webserver/ZimHostFragment.kt
+++ app/src/main/java/org/kiwix/kiwixmobile/webserver/ZimHostFragment.kt
@@ -95,6 +95,7 @@ class ZimHostFragment : BaseFragment(), ZimHostCallbacks, ZimHostContract.View {
   override val fragmentToolbar: Toolbar? by lazy {
     activityZimHostBinding?.root?.findViewById(R.id.toolbar)
   }
+  private var allBooks: List<BooksOnDiskListItem>? = null
   private val selectedBooksPath: ArrayList<String>
     get() {
       return booksAdapter.items
@@ -111,6 +112,16 @@ class ZimHostFragment : BaseFragment(), ZimHostCallbacks, ZimHostContract.View {
           }
         }
         as ArrayList<String>
+    }
+
+  private val selectedBooks: List<BooksOnDiskListItem>
+    get() {
+      return booksAdapter.items
+        .filter(BooksOnDiskListItem::isSelected)
+        .filterIsInstance<BookOnDisk>()
+        .map {
+          it
+        }
     }
 
   override fun onCreateView(
@@ -356,6 +367,7 @@ class ZimHostFragment : BaseFragment(), ZimHostCallbacks, ZimHostContract.View {
     activityZimHostBinding?.startServerButton?.setBackgroundColor(
       ContextCompat.getColor(requireActivity(), R.color.stopServerRed)
     )
+    booksAdapter.items = selectedBooks
     bookDelegate.selectionMode = SelectionMode.NORMAL
     booksAdapter.notifyDataSetChanged()
   }
@@ -382,6 +394,7 @@ class ZimHostFragment : BaseFragment(), ZimHostCallbacks, ZimHostContract.View {
     activityZimHostBinding?.startServerButton?.setBackgroundColor(
       ContextCompat.getColor(requireActivity(), R.color.startServerGreen)
     )
+    allBooks?.let { booksAdapter.items = it }
     bookDelegate.selectionMode = SelectionMode.MULTI
     booksAdapter.notifyDataSetChanged()
   }
@@ -446,6 +459,7 @@ class ZimHostFragment : BaseFragment(), ZimHostCallbacks, ZimHostContract.View {
 
   override fun addBooks(books: List<BooksOnDiskListItem>) {
     booksAdapter.items = books
+    allBooks = books
   }
 
   override fun onIpAddressValid() {
